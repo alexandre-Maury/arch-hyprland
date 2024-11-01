ca00e41eab1931bc8312ba7d4991dea379a41725
alexandre-Maury <alexandre_maury@outlook.com>
2024-11-02 00:24:08 +0100

    maj

--- install.sh
+++ install.sh
@@ -105,10 +105,10 @@ yay -S alacritty nautilus rofi-wayland dunst grim slurp \
 yay -S curl tar wget cmake meson ninja gcc make glibc cairo libzip librsvg tomlplusplus gdb pugixml gbm libdrm libpipewire sdbus-cpp wayland wayland-protocols scdoc \
     qt5-wayland qt6-wayland libjpeg-turbo libwebp pango pkgconf libglvnd pam udis-86 libxcb xcb-proto xcb-util xcb-util-keysyms \
     libxfixes libx11 libxcomposite xorg-xinput libxrender pixman libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info \
-    cpio xcb-util-errors otf-font-awesome ttf-jetbrains-mono waybar --noconfirm
+    cpio xcb-util-errors waybar --noconfirm
 
 # https://github.com/Jannomag/Yaru-Colors/tree/master
-# yay -S humanity-icon-theme yaru-icon-theme hicolor-icon-theme
+# yay -S otf-font-awesome ttf-jetbrains-mono
 
 
 ##############################################################################
